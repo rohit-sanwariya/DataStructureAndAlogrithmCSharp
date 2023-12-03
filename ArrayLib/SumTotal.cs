db142122c678e1afb8ce99615724c8c86f875cff
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2023-12-03 16:57:01 +0530

    Add project files.

---
+++ ArrayLib/SumTotal.cs
@@ -0,0 +1,13 @@
+﻿namespace ArrayLib
+{
+    public class SumTotal
+    {
+        int[] numbers = { 1, 2, 3, 4 };
+
+        public SumTotal()
+        {
+         
+        }
+
+    }
+}
---
+++ DsaInCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using ArrayLib;
+
+namespace DsaInCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           SumTotal sumTotal = new SumTotal();
+        }
+    }
+}
