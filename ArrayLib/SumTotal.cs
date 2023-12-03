53abde363f77382f07ea7fdd39c8f84f81810899
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2023-12-03 17:15:09 +0530

    Sum total solved

--- ArrayLib/SumTotal.cs
+++ ArrayLib/SumTotal.cs
@@ -1,12 +1,28 @@
-﻿namespace ArrayLib
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace ArrayLib
 {
     public class SumTotal
     {
-        int[] numbers = { 1, 2, 3, 4 };
+        int[] numbers = { 1, 7, 2, 3, 4 };
+        int target = 11;
+        public SumTotal() {
 
-        public SumTotal()
+            Console.WriteLine(GetSumTotal().Item1+" "+GetSumTotal().Item2);
+        }
+        public (int,int) GetSumTotal()
         {
-         
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if(dict.ContainsKey(target - numbers[i]))
+                {
+                    return (dict[target - numbers[i]] ,i );
+                }
+                dict.Add(numbers[i], i);
+            }
+            return (-1, -1);
+
         }
 
     }
